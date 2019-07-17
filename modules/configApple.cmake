9ce73c9b0f37eeba48ff95cdb947fd4f602c5132
c-mendoza <c@matadata.com>
2019-07-17 16:38:52 -0400

    Compiler and architecture to system default

--- modules/configApple.cmake
+++ modules/configApple.cmake
@@ -1,15 +1,15 @@
 
 # ============================================================================
 # ------------------------------ Compiler Flags ------------------------------
-set(CMAKE_C_COMPILER "/usr/bin/clang")
+# set(CMAKE_C_COMPILER "/usr/bin/clang")
 set(CMAKE_C_FLAGS "") # -x objective-c
 
-set(CMAKE_CXX_COMPILER "/usr/bin/clang++")
+# set(CMAKE_CXX_COMPILER "/usr/bin/clang++")
 set(CMAKE_CXX_FLAGS "-std=c++11 -stdlib=libc++ -D__MACOSX_CORE__") # Removed "-stdlib=libstdc++
 set_source_files_properties(${OF_SOURCE_FILES} PROPERTIES COMPILE_FLAGS "-x objective-c++")
 
 #set(CMAKE_OSX_ARCHITECTURES i386)
-set(CMAKE_OSX_ARCHITECTURES x86_64)
+# set(CMAKE_OSX_ARCHITECTURES x86_64)
 add_compile_options(-Wno-deprecated-declarations)
 
 
