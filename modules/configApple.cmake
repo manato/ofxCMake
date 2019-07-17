dc02b54ff1698ae71740e96a6c4486812143db8d
c-mendoza <c@matadata.com>
2019-07-17 16:39:41 -0400

    CC Cache

--- modules/configApple.cmake
+++ modules/configApple.cmake
@@ -12,6 +12,12 @@ set_source_files_properties(${OF_SOURCE_FILES} PROPERTIES COMPILE_FLAGS "-x obje
 # set(CMAKE_OSX_ARCHITECTURES x86_64)
 add_compile_options(-Wno-deprecated-declarations)
 
+find_program(CCACHE_FOUND ccache)
+if(CCACHE_FOUND)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+endif(CCACHE_FOUND)
+
 
 # ============================================================================
 # ------------------------------ Compile and Link ----------------------------
